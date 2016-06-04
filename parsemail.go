8cc6b36924f1e853881dab5e909701d6447c7872
Steve Crook <steve@mixmin.net>
2016-06-04 21:03:42 +0100

    Add Orig_To and To options.
    This actually involves an almost complete rewrite!

--- parsemail.go
+++ parsemail.go
@@ -8,6 +8,78 @@ import (
 	"regexp"
 )
 
+var (
+	reClient *regexp.Regexp
+	reOrigTo *regexp.Regexp
+	reSender *regexp.Regexp
+	reTo     *regexp.Regexp
+
+	flagUniq    bool
+	flagClient  bool
+	flagOrigTo  bool
+	flagSender  bool
+	flagTo      bool
+	flagLogfile string
+)
+
+func init() {
+	// Declare and compile Regular Expressions
+	var err error
+	reTxtClient := "client=([^, \n]+)"
+	reTxtOrigTo := "orig_to=<([^>]+)"
+	reTxtSender := "from=<([^>]+)"
+	reTxtTo := " to=<([^>]+)"
+	reClient, err = regexp.Compile(reTxtClient)
+	errTest(err)
+	reOrigTo, err = regexp.Compile(reTxtOrigTo)
+	errTest(err)
+	reSender, err = regexp.Compile(reTxtSender)
+	errTest(err)
+	reTo, err = regexp.Compile(reTxtTo)
+	errTest(err)
+
+	// Specify command line flags
+	flag.BoolVar(&flagUniq, "uniq", false, "Dedupe results")
+	flag.BoolVar(
+		&flagClient,
+		"client",
+		false,
+		"Match Client MTA's",
+	)
+	flag.BoolVar(
+		&flagOrigTo,
+		"origto",
+		false,
+		"Match Original Recipient (orig_to)",
+	)
+	flag.BoolVar(
+		&flagSender,
+		"sender",
+		false,
+		"Match Envelope senders",
+	)
+	flag.StringVar(
+		&flagLogfile,
+		"log",
+		"/var/log/mail.info",
+		"Mail logfile to parse",
+	)
+	flag.BoolVar(
+		&flagTo,
+		"to",
+		false,
+		"Match Recipient (to)",
+	)
+	flag.Parse()
+}
+
+// Kludgy panic on error function
+func errTest(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 // IsMemberStr tests for the membership of a string in a slice
 func IsMemberStr(s string, slice []string) bool {
 	for _, n := range slice {
@@ -18,55 +90,11 @@ func IsMemberStr(s string, slice []string) bool {
 	return false
 }
 
-// makeSenderRegexp compiles a Regex suitable for Sender matching
-func makeSenderRegexp() (reSender *regexp.Regexp) {
-	reSender, err := regexp.Compile("from=<([^>]+)")
-	if err != nil {
-		panic(err)
-	}
-	return
-}
-
-// makeClientRegexp compiles a Regex suitable for Client matching
-func makeClientRegexp() (reClient *regexp.Regexp) {
-	reClient, err := regexp.Compile("client=([^, \n]+)")
-	if err != nil {
-		panic(err)
-	}
-	return
-}
-
 /*
 	Client  - Domain name of the sending server
 	Sender - Email address of the Envelope Sender
 */
 func main() {
-	var flagUniq bool
-	var flagClient bool
-	var flagLogfile string
-	flag.BoolVar(&flagUniq, "uniq", false, "Dedupe results")
-	flag.BoolVar(
-		&flagClient,
-		"client",
-		false,
-		"Match Clients instead of Senders",
-	)
-	flag.StringVar(
-		&flagLogfile,
-		"log",
-		"/var/log/mail.info",
-		"Mail logfile to parse",
-	)
-	flag.Parse()
-
-	// Define the regex to match log records against
-	var hit *regexp.Regexp
-	if flagClient {
-		hit = makeClientRegexp()
-	} else {
-		hit = makeSenderRegexp()
-	}
-
 	// Open the logfile for reading
 	f, err := os.Open(flagLogfile)
 	if err != nil {
@@ -74,11 +102,26 @@ func main() {
 	}
 	defer f.Close()
 
-	var uniqChk []string
+	var uniqChk []string // Used when testing for unique results
 	scanner := bufio.NewScanner(f)
+	// Iterate file line-by-line
 	for scanner.Scan() {
 		line := scanner.Text()
-		res := hit.FindStringSubmatch(line)
+		var res []string // List of sub-matches on a line
+		if flagClient {
+			res = reClient.FindStringSubmatch(line)
+		}
+		if flagOrigTo && len(res) == 0 {
+			res = reOrigTo.FindStringSubmatch(line)
+		}
+		if flagSender && len(res) == 0 {
+			res = reSender.FindStringSubmatch(line)
+		}
+		// to and orig_to always occur on the same line so this will
+		// never be parsed if orig_to is tested.
+		if flagTo && len(res) == 0 {
+			res = reTo.FindStringSubmatch(line)
+		}
 		if len(res) < 1 {
 			// No submatches found. Move on!
 			continue
